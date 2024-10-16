cc4c44076758a5809ff51ae0e52fc6ca2ae70cb4
dhruvinvaghani001 <dhruvinvaghani0011@gmail.com>
2024-10-16 16:42:27 +0530

    add:ui for small devices in navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,20 @@
 "use client";
 import { cn } from "@/lib/utils";
+import { Category } from "@/types";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
-const Navigation = ({ data }: { data: any }) => {
+const Navigation = ({ data }: { data: Category[] }) => {
   const pathname = usePathname();
 
   const routes = data.map((item) => {
@@ -17,22 +27,70 @@ const Navigation = ({ data }: { data: any }) => {
 
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      {routes.map((item) => {
-        return (
-          <Link
-            href={item.href}
-            className={cn(
-              "text-sm font-medium transition-colors hover:text-black ",
-              routes.active ? "text-black" : "text-neutral-500"
-            )}
-            key={item.href}
-          >
-            {item.label}
-          </Link>
-        );
-      })}
+      <div className="inline lg:hidden">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Category</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                  {routes.map((component) => (
+                    <ListItem
+                      key={component.href}
+                      title={component.label}
+                      href={component.href}
+                    ></ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div className="hidden lg:inline lg:mx-6 lg:space-x-4 ">
+        {routes.map((item) => {
+          return (
+            <Link
+              href={item.href}
+              className={cn(
+                "text-sm font-medium transition-colors hover:text-black ",
+                item.active ? "text-black" : "text-neutral-500"
+              )}
+              key={item.href}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
+      </div>
     </nav>
   );
 };
 
 export default Navigation;
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
